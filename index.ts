f017b8002880d7297ba8e3cfe0260c37dacd3ce3
shoshinprograms <shoshinprograms@gmail.com>
2021-09-05 23:01:49 +1000

    Updates, no tests yet

--- index.ts
+++ index.ts
@@ -1,17 +1,57 @@
 import Discord from "discord.js";
-import { accept, addmembers, leaveteam, registerteam, reject } from "./src/command_processor";
-import { bot_token } from "./src/config";
+import { accept, addmembers, deleteAll, leaveteam, registerteam, reject } from "./src/command_processor";
+import { bot_token, msg_channel_id } from "./src/config";
+import sqlite3, { OPEN_CREATE, OPEN_READWRITE } from "sqlite3";
+import {open} from "sqlite";
 
+(async () => {
 const client = new Discord.Client();
+// Defined in client.on('ready',...)
+// TODO: handle opening when there's no file and handle when there's a file
+let db = await open({
+  filename: "./andpuzzsoc.db",
+  driver: sqlite3.Database,
+  mode: OPEN_READWRITE
+}).then(db1 => db1, err => {console.log("ERROR: no database found, creating one"); return null;});
+// When database does not exist create the tables
+if (db == null){
+  db = await open({
+    filename: "./andpuzzsoc.db",
+    driver: sqlite3.Database,
+    mode: OPEN_READWRITE | OPEN_CREATE
+  }).then(db1 => db1, err => {console.log("ERROR: cannot create db, " + err); return null;});
+  if (db !== null){
+    await db.exec(`CREATE TABLE puzzUsers (
+      discord_id TEXT PRIMARY KEY,
+      puzz_team_id TEXT
+    )`);
+    // TODO: creator_id can be stale if the creator leaves the team while having members in them
+    await db.exec(`CREATE TABLE puzzTeams (
+      puzz_team TEXT PRIMARY KEY,
+      puzz_team_id TEXT NOT NULL,
+      creator_id TEXT NOT NULL,
+      parent_channel_id TEXT NOT NULL,
+      text_channel_id TEXT NOT NULL,
+      voice_channel_id TEXT NOT NULL
+    )`);
+    await db.exec(`CREATE TABLE pendingInvitations (
+      discord_id TEXT NOT NULL,
+      puzz_team TEXT NOT NULL,
+      puzz_team_id TEXT NOT NULL
+    )`)
+  }
+}
 
-client.on('ready', () => {
+
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user?.tag}!`);
   });
 
 client.on('message', async msg => {
+  if (msg.channel.id !== msg_channel_id) return;
   // obtain prefix
   const prefix = "!";
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  if (!msg.content.startsWith(prefix) || msg.author.bot || !db) return;
 
 	const args = msg.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift()?.toLowerCase();
@@ -22,32 +62,68 @@ client.on('message', async msg => {
     // Create voice channel
     // Create text channel
     // Create role
-    const res = await registerteam(client, msg, args);
+    if (args.length != 1) {
+      msg.reply('register team can only accept 1 argument');
+      return;
+    }
+    const res = await registerteam(client, msg, args, db);
   } else if (command === 'addmembers'){
     // Wait for targets to accept or reject
-    // Add to queue or hashmap of pending invitations for their respective user and members 
-    const res = await addmembers(client, msg, args);
+    // Add to queue or hashmap of pending invitations for their respective user and members
+    if (args.length == 0) {
+      msg.reply('addmembers needs 1 or more members to be mentioned');
+      return;
+    }
+    const res = await addmembers(client, msg, args,db);
   } else if (command === 'accept') {
-    // Look for user's id in the queue or hashmap and update their role
-    const res = await accept(client, msg);
+    if (args.length != 1) {
+      msg.reply('accept only accepts teamname');
+      return;
+    }
+    const res = await accept(client, msg,args, db);
   } else if (command === 'reject') {
-    // Look for user's id in the queue or hashmap and don't update their role
-    const res = await reject(client, msg);
+    if (args.length != 1) {
+      msg.reply('reject only rejects teamname');
+      return;
+    }
+    const res = await reject(client, msg,args, db);
   } else if (command === 'leaveteam'){
-    const res  = await leaveteam(client,msg);
+    if (args.length != 0) {
+      msg.reply('leaveteam does not accept arguments');
+      return;
+    }
+    const res  = await leaveteam(client,msg,args, db);
+  } else if (command === 'deleteall'){
+    const res = await deleteAll(client, msg, args, db);
   } else if (command === 'help') {
     // Display commands
     msg.channel.send(`
     \`\`\`
-!registerteam [teamname]          Create voice & text channels & roles for your teamname
-!addmembers [@user1 @user2 ...]   Add @users to your team
-!accept                           Accept team invitation
-!reject                           Reject team invitation
-!leaveteam                        Leave your current team
-!help                             Display this menu
+!registerteam teamname
+    eg. !registerteam poggers
+    Create voice & text channels & roles for your teamname
+
+!addmembers @user1 @user2 ...
+    eg. !addmembers @despacito @dream
+    Add @users to your team
+
+!accept teamname
+    eg. !accept poggers
+    Accept team invitation
+
+!reject teamname
+    eg. !reject poggers
+    Reject team invitation
+
+!leaveteam
+    Leave your current team
+
+!help
+    Display this menu
     \`\`\`
     `)
   }
 });
   
 client.login(bot_token);
+})()
--- src/command_processor.ts
+++ src/command_processor.ts
@@ -1,5 +1,7 @@
 import Discord from "discord.js";
-import { guild_id} from "./config";
+import { Database } from "sqlite";
+import { guild_id, hint_givers_id} from "./config";
+import sqlite3 from "sqlite3";
 
 
 // Create role
@@ -9,11 +11,20 @@ import { guild_id} from "./config";
 // global state of the regitered user's with their team
 type DiscordId= string;
 // create a persistent storage for userTeams
-let userTeams :Record<DiscordId,Discord.Role>= {};
-async function registerteam(client: Discord.Client, msg:Discord.Message, args: string[]){
+// TODO: Add the execs to the channel too
+async function registerteam(client: Discord.Client, msg:Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>){
+    let row = await db.get('SELECT * FROM puzzUsers WHERE discord_id = ?', msg.author.id);
+    if (row && row["puzz_team_id"] !== null) {
+        msg.reply("You're already in a team.")
+        return;
+    }
+    row = await db.get('SELECT * FROM puzzTeams WHERE puzz_team = ?', args[0]);
+    if (row && row["puzz_team"] !== undefined){
+        msg.reply("Team name already exists.")
+        return;
+    }
     // TODO: handle color registration for teams and maybe modifiably change their colors when created
     const guild = await client.guilds.fetch(guild_id);
-    // TODO: handle when team_name is undefined
     const team_name = args[0];
     const new_role = await guild.roles.create({
         data:{
@@ -31,6 +42,10 @@ async function registerteam(client: Discord.Client, msg:Discord.Message, args: s
         {
             id: new_role.id,
             allow: ['VIEW_CHANNEL']
+        },
+        {
+            id: hint_givers_id,
+            allow: ['VIEW_CHANNEL']
         }
     ];
     const blank_category = await guild.channels.create(`team-${team_name}`,{
@@ -47,73 +62,128 @@ async function registerteam(client: Discord.Client, msg:Discord.Message, args: s
         parent: blank_category.id,
     })
     const new_category = await blank_category.overwritePermissions(perm_ow);
-    userTeams[msg.author.id] = new_role;
+    await db.run(
+        'INSERT OR REPLACE INTO puzzTeams (puzz_team, puzz_team_id, creator_id, parent_channel_id, text_channel_id, voice_channel_id) VALUES (?,?,?,?,?,?)',
+        `${team_name}`,
+        `${new_role.id}`,
+        `${msg.author.id}`,
+        `${new_category.id}`,
+        `${new_text_channel.id}`,
+        `${new_voice_channel.id}`
+    )
+    await db.run(
+        'INSERT OR REPLACE INTO puzzUsers (discord_id, puzz_team_id) VALUES (?,?)',
+        `${msg.author.id}`,
+        `${new_role.id}`
+    )
+    let bot_id = client.user?.id
+    if (bot_id) await guild.members.resolve(bot_id)?.roles.add(new_role);
+    // TODO add roles into execs too
     msg.channel.send(`Team ${team_name} has been registered, and lead by ${msg.author.tag}`)
     // TODO cleanup code if any of the awaits fail
 }
 
-// global state store for pending accepts
-// TODO: Create a persistent storage for pendingAccepts
-let pendingAccepts :Record<DiscordId,Discord.Role>= {};
-async function addmembers(client: Discord.Client, msg: Discord.Message, args: string[]){
+// Create a persistent storage for pendingAccepts
+async function addmembers(client: Discord.Client, msg: Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>){
     const mention_ids = args.map(mention_id => mention_id.slice(2,-1))
                             .map(mention_id => mention_id.startsWith('!') ? mention_id.slice(1) : mention_id);
-    const user_tag = args[0];
-    // TODO: Do I have to check if the members exists?
-    const guild = await client.guilds.fetch(guild_id);
-    const role = userTeams[msg.author.id];
-    if (role !== undefined){
+    let row = await db.get('SELECT * FROM puzzUsers WHERE discord_id = ?', msg.author.id);
+    // TODO: Ascertain if team/role exists
+    if (row && row["puzz_team_id"] !== null){
+        let res = await db.get('SELECT * FROM puzzTeams WHERE puzz_team_id = ?', row["puzz_team_id"]);
+        let role = res["puzz_team_id"];
         // TODO: is the mention_id already in a team?
-        mention_ids.forEach(mention_id => {
-            pendingAccepts[mention_id] = role;
+        mention_ids.forEach(async(mention_id) => {
+            await db.run(
+                'INSERT OR IGNORE INTO puzzUsers (discord_id, puzz_team_id) VALUES (?,NULL)',
+                `${mention_id}`,
+            )
+            await db.run(
+                'INSERT INTO pendingInvitations (discord_id,puzz_team, puzz_team_id) VALUES (?,?,?)',
+                `${mention_id}`,
+                `${res["puzz_team"]}`,
+                `${role}`
+            )
         });
-        // TODO return a message to the user
-        msg.channel.send(`Awaiting acceptance or rejection of ${user_tag}`);
+        msg.channel.send(`Awaiting acceptance or rejection of members`);
+    }else{
+        msg.reply(`You are not part of any team. Create or join a team.`);
     }
 }
 
-async function accept(client: Discord.Client, msg:Discord.Message) {
-    const role = pendingAccepts[msg.author.id];
-    const isInTeam = userTeams[msg.author.id]
-    if (isInTeam){
-        msg.channel.send(`You are already in team ${isInTeam.name}`);
+// given a team name
+// if team name is in user's pending then accept and change user teamname col
+// Otherwise reply no teams
+async function accept(client: Discord.Client, msg:Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>) {
+    const user_row = await db.get('SELECT * FROM puzzUsers WHERE discord_id = ?', msg.author.id);
+    const pendingInvites_row = await db.get('SELECT * FROM pendingInvitations WHERE puzz_team = ? AND discord_id = ?', args[0], msg.author.id);
+    if (user_row["puzz_team_id"] !== null){
+        msg.channel.send(`You are already in a team`);
     }
-    else if (role) {
-        msg.member?.roles.add(role);
-        userTeams[msg.author.id] = role;
-        msg.channel.send(`Accepting ${role.name}'s invitation`);
-        delete pendingAccepts[msg.author.id];
+    else if (pendingInvites_row !== undefined) {
+        let puzz_team = await db.get('SELECT * FROM puzzTeams WHERE puzz_team_id = ?',pendingInvites_row["puzz_team_id"]);
+        await msg.member?.roles.add(pendingInvites_row["puzz_team_id"]);
+        msg.channel.send(`Accepting ${puzz_team["puzz_team"]}'s invitation`);
+        await db.run ('UPDATE puzzUsers SET puzz_team_id = ? WHERE discord_id = ?',pendingInvites_row["puzz_team_id"], msg.author.id);
+        await db.run('DELETE FROM pendingInvitations WHERE discord_id = ?', msg.author.id);
     } else {
         msg.channel.send(`No teams to accept, please ask their members to invite you.`)
     }
 }
-async function reject(client: Discord.Client, msg:Discord.Message) {
-    const role = pendingAccepts[msg.author.id];
-    if (role) {
-        delete pendingAccepts[msg.author.id];
-        msg.channel.send(`Rejecting ${role.name}'s invitation`);
+async function reject(client: Discord.Client, msg:Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>) {
+    const pendingInvites_row = await db.get('SELECT * FROM pendingInvitations WHERE puzz_team = ?', args[0]);
+    if (pendingInvites_row !== undefined) {
+        await db.run('DELETE FROM pendingInvitations WHERE discord_id = ? AND puzz_team = ?', msg.author.id, args[0]);
+        msg.channel.send(`Rejecting ${pendingInvites_row["puzz_team"]}'s invitation`);
     } else {
         msg.channel.send(`No teams to reject, please ask their members to invite you.`)
     }
 }
 
-// TODO: Individually remove teams and their channels
-// TODO: Collectivelly remove teams and their channels
-// TODO: Remove all teams and their channels
+// Individually remove teams and their channels
+async function deleteOne(client: Discord.Client, puzz_team_id : string,db: Database<sqlite3.Database, sqlite3.Statement>){
+    const guild = await client.guilds.fetch(guild_id);
+    let row = await db.get('SELECT * FROM puzzTeams WHERE puzz_team_id = ?', puzz_team_id);
+    await guild.channels.resolve(row["text_channel_id"])?.delete();
+    await guild.channels.resolve(row["voice_channel_id"])?.delete();
+    await guild.channels.resolve(row["parent_channel_id"])?.delete();
+    await guild.roles.resolve(row["puzz_team_id"])?.delete();
+}
+
+
+// Remove all teams and their channels and roles
+async function deleteAll(client: Discord.Client, msg:Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>){
+    const rowsCount = await db.each(
+        'SELECT * FROM puzzTeams',
+        async (err,row) => {
+            if (err) console.log("ERROR: deleteAll" + err);
+            const guild = await client.guilds.fetch(guild_id);
+            await guild.channels.resolve(row["text_channel_id"])?.delete();
+            await guild.channels.resolve(row["voice_channel_id"])?.delete();
+            await guild.channels.resolve(row["parent_channel_id"])?.delete();
+            await guild.roles.resolve(row["puzz_team_id"])?.delete();
+        }
+    )
+    await db.run('UPDATE puzzUsers SET puzz_team_id = NULL');
+    await db.run('DELETE FROM puzzTeams');
+    msg.reply("Deleted all roles");
+}
 
 
 // TODO: Ping an exec for help for a puzzle
-// TODO: what happens when a team has no members?
 // User quit team
-async function leaveteam(client: Discord.Client, msg:Discord.Message) {
-    const role = userTeams[msg.author.id];
+async function leaveteam(client: Discord.Client, msg:Discord.Message, args: string[],db: Database<sqlite3.Database, sqlite3.Statement>) {
+    const user_row = await db.get('SELECT * FROM puzzUsers WHERE discord_id = ?', msg.author.id);
+    let role = user_row["puzz_team_id"];
     if (role){
         msg.member?.roles.remove(role);
-        delete userTeams[msg.author.id];
-        msg.channel.send(`${msg.author.tag} has been removed from team ${role.name}`);
+        await db.run('UPDATE puzzUsers SET puzz_team_id = NULL WHERE discord_id = ?', msg.author.id);
+        msg.channel.send(`${msg.author.tag} has left the team`);
+        let row = await db.get('SELECT COUNT(*) FROM puzzUsers WHERE puzz_team_id = ?', role);
+        if (row["COUNT(*)"] === 0) deleteOne(client, role, db);
     } else {
         msg.channel.send(`You are not in a team yet.`);
     }
 }
 
-export {registerteam, addmembers, accept, reject, leaveteam};
+export {registerteam, addmembers, accept, reject, leaveteam, deleteAll};
